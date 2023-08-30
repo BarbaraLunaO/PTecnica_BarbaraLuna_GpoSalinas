573e4238a21c261994ca15c4227d62e7e16b466f
Barbara Luna <barbara.alo@hotmail.com>
2023-08-30 17:19:02 -0600

    feat Se agrega el modelo de la tabla consultas y la relacion con la tabla usuarios

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .conexion import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__= 'usuario'
@@ -7,3 +8,12 @@ class User(Base):
     username = Column(String(20))
     password = Column(String(10))
     rol = Column(String(20))
+    consultas = relationship("Consultas", back_populates="user")
+
+class Consultas(Base):
+    __tablename__= 'consultas'
+    id = Column(Integer, primary_key=True, index=True)
+    id_user = Column(Integer, ForeignKey('usuario.id'))
+    date = Column(String(20))
+    search = Column(String(20))
+    user = relationship("User", back_populates="consultas")
